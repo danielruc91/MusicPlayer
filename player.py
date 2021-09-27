e9684444414fecca3d6299f8d5affb497d50f882
Chuanning <danielruc91@gmail.com>
2021-09-26 21:42:10 -0400

    Adding Server

---
+++ player.py
@@ -0,0 +1,80 @@
+import pathlib
+import random
+import subprocess
+from typing import List, Union
+import os
+import argparse
+import logging
+import config
+
+
+def isSonata(f: str) -> bool:
+    return len(f) > 7 and f[-7] == ' ' and f[-6].isdigit()
+
+
+class player:
+    """ Playing the music in a directory (and its subdirectories)
+    """
+
+    def __init__(self, path) -> None:
+        self.keepplay = True
+        self.P = None
+        self.next = []
+
+        self.musics = []
+        tmp = {}
+        for dirpath, _, filenames in os.walk(path):
+            for name in filenames:
+                if name.endswith(".flac"):
+                    f = os.path.join(dirpath, name)
+                    if not isSonata(f):
+                        self.musics.append([f])
+                    else:
+                        tmp.setdefault(f[0:-7], []).append(f)
+        for key in tmp:
+            self.musics.append(sorted(tmp[key]))
+
+    def start(self):
+        """ Blockingly play musics
+        """
+        if 0 == len(self.musics):
+            raise "Did not find any music file under this folder."
+
+        if self.isplaying():
+            return
+
+        self.keepplay = True
+        while self.keepplay:
+            m = self.NextSong()
+            logging.info(m)
+            self.P = subprocess.Popen(
+                ["ffplay", "-hide_banner", "-autoexit", "-nodisp", m])
+            self.P.wait()
+
+    def NextSong(self) -> str:
+        """ Pick the next music file (or list of files) to play
+        """
+        if 0 == len(self.next):
+            self.next = random.choice(self.musics).copy()
+        return self.next.pop()
+
+    def stop(self):
+        self.keepplay = False
+        self.next = []
+        self.P.kill()
+        self.P.wait()
+        self.P = None
+
+    def isplaying(self):
+        return self.P != None
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Simple music player.")
+    parser.add_argument("path", metavar="path", nargs='?', default=config.DefaultPath,
+                        help="Path to music folder, default to system environment variable")
+    arguments = parser.parse_args()
+    p = player(arguments.path)
+
+    p.start()
--- server.py
+++ server.py
@@ -1,1 +1,81 @@
+"""HTTP Server Handeling request to control music playing.
+"""
+
+
+import multiprocessing
+import os
+
 import http.server
+import time
+
+
+from player import player
+import config
+from threading import Thread
+
+with open("index.html", 'r') as f:
+    INDEXPAGE = f.read().encode()
+
+
+class controller:
+    def __init__(self) -> None:
+        self.player = player(config.DefaultPath)
+        self.T = None
+
+    def start(self):
+        if self.T == None:
+            self.T = Thread(target=self.player.start)
+            self.T.start()
+
+    def stop(self):
+        if self.player.isplaying():
+            self.player.stop()
+            self.T.join()
+            self.T = None
+
+    def next(self):
+        self.stop()
+        self.start()
+
+
+CON = controller()
+
+
+class MPHandler(http.server.BaseHTTPRequestHandler):
+    """Simple HTTP request handler with GET/HEAD/POST commands.
+
+    Get: Return a simple index page
+    Post:
+        Start: start playing
+        Stop: stops playing
+        Next: jump to next song
+
+    """
+
+    def do_GET(self):
+        """Serve a GET request."""
+        command = str(self.path)
+
+        print(command)
+
+        if command == r"/Start?":
+            CON.start()
+        elif command == r"/Stop?":
+            CON.stop()
+        elif command == r"/Next?":
+            CON.next()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(INDEXPAGE)
+
+
+def test(HandlerClass=MPHandler,
+         ServerClass=http.server.HTTPServer):
+    http.server.test(HandlerClass, ServerClass)
+
+
+if __name__ == '__main__':
+    test()
